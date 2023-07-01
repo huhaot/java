66caa0bcfda45f934c7318a1899910b137ed453f
huhaotian <869393590@qq.com>
2023-07-01 20:36:57 +0800

    新增了部分代码

--- frist try with java/src/Helloworld.java
+++ frist try with java/src/Helloworld.java
@@ -1,2 +1,12 @@
+
 public class Helloworld {
+    //main >psvm
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+//sout
+        System.out.println(10);
+        //10.sout
+        System.out.println(10);
+    }
 }
