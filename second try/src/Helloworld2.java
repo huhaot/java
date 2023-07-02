adba4576c902ae0c28a10ab8739a50369f176937
huhaotian <869393590@qq.com>
2023-07-02 14:26:00 +0800

    笔记

--- second try/src/Helloworld2.java
+++ second try/src/Helloworld2.java
@@ -6,4 +6,11 @@
  * Time: 14:08
  */
 public class Helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("nigger");
+    }
+//javac -encoding utf-8
+// 改编译能输入中文
+    //javadoc -d myHello -author -version -encoding utf-8 Helloworld2.java
+    //解释代码
 }
