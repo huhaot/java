4409fbb49411a8b2ba64c136a0d3a685e88152c7
huhaotian <869393590@qq.com>
2023-07-02 15:36:53 +0800

    更新

---
+++ warehouse/src/Helloworld2.java
@@ -0,0 +1,38 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: hht
+ * Date: 2023-07-02
+ * Time: 14:08
+ */
+public class Helloworld2 {
+    public static void main(String[] args) {
+
+        System.out.println("nigger");
+        System.out.println();
+
+    }
+
+}
+
+
+/**注意
+ * 1.定义常量大写
+ * 2.System.out.printf,%s表示字符串，%d表示整数，%f表示浮点数
+ */
+
+/**快捷键
+ * 1,main和 psvm
+ * public static void main(String[] args) {
+ *2,sout
+ * System.out.println();
+ *     }
+ */
+
+/**cmd长代码
+ * javac -encoding utf-8
+ *  改编译能输入中文
+ * javadoc -d myHello -author -version -encoding utf-8 Helloworld2.java
+ *  解释代码帮助手册
+ *
+ */
