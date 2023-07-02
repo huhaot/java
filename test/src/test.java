cb41ac23f492d3771f70d85132f3d673e05d2762
huhaotian <869393590@qq.com>
2023-07-02 15:36:12 +0800

    更新

---
+++ test/src/test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: hht
+ * Date: 2023-07-02
+ * Time: 14:45
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.printf("%s/n","hht");
+        System.out.println();
+    }
+    }
+
